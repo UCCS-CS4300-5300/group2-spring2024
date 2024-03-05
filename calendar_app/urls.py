99990d901126e4ada128d984b6bcb76e01f27b2b
Nick <chesinick@gmail.com>
2024-03-05 13:35:59 -0700

    Added views and html for register and login. Added forms for CustomUserCreationForm

--- calendar_app/urls.py
+++ calendar_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='calendar_app/login.html'), name='login'),
 ]
--- calendar_app/views.py
+++ calendar_app/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def index(request):
     # Render index.html
     return render( request, 'calendar_app/index.html')
+
+# Registration form / login
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('calendar_app/index.html') 
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'calendar_app/register.html', {'form': form})
