b72701cc804982641ab8f9f34492b11208a810c1
Nick <chesinick@gmail.com>
2024-03-05 13:59:30 -0700

    Added the proper reverse lookup for login / register / homepage

--- calendar_app/urls.py
+++ calendar_app/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
+    path('home/', views.index, name='home'),
+    path('register/calendar_app/index.html', views.index, name='index'),
     path('login/', LoginView.as_view(template_name='calendar_app/login.html'), name='login'),
 ]
