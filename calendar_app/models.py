de40c9948069f0a2138b46f75e0a9e282da07b11
Austin <austinybyrdy@gmail.com>
2024-03-04 20:03:07 -0700

    Model for Tasks

--- calendar_app/models.py
+++ calendar_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+    deadlineDay = models.DateField()
+    deadlineTime = models.TimeField()
+    category = models.OneToOneField(Category)
+    duration = models.DurationField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.BooleanField()
+    def __str__(self):
+        return self.name
+
+
