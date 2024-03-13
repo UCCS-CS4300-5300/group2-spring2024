685cd48da4b2a4f7c011b04c8f8994a5b1d6a720
mtulepkaliev <moses1.tule@gmail.com>
2024-03-12 19:53:00 -0600

    Fixed month tsak display test case

--- calendar_app/tests.py
+++ calendar_app/tests.py
@@ -31,24 +31,20 @@ class MonthTaskDisplay(TestCase):
         # Make test user to assign tasks to
         email = "testuser@uccs.edu"
         self.customUser = CustomUser.objects.create_user(username="testuser1", email=email, password="testpassword123")
-        
-        # Categories; category must be unique according to current Task model
-        self.newCat = Category.objects.create(category=1)
-        self.newCat2 = Category.objects.create(category=2)
-        self.newCat3 = Category.objects.create(category=3)
+
 
         # Make tasks
         self.newTask = Task.objects.create(name='TestTask1',description='TestDesc1',
                                            deadlineDay=date(2024,3,12),deadlineTime=time(23,59),
-                                           category=self.newCat,duration=timedelta(days=0, hours=2),
+                                           category=None,duration=timedelta(days=0, hours=2),
                                            user=self.customUser,status=False)
         self.newTask = Task.objects.create(name='TestTask2',description='TestDesc2',
                                            deadlineDay=date(2024,3,26),deadlineTime=time(23,59),
-                                           category=self.newCat2,duration=timedelta(days=0, hours=1),
+                                           category=None,duration=timedelta(days=0, hours=1),
                                            user=self.customUser,status=False)
         self.newTask = Task.objects.create(name='TestTask3',description='TestDesc3',
                                            deadlineDay=date(2024,3,30),deadlineTime=time(23,59),
-                                           category=self.newCat3,duration=timedelta(days=0, hours=3),
+                                           category=None,duration=timedelta(days=0, hours=3),
                                            user=self.customUser,status=False)
     
     def test_month_task_display(self):
